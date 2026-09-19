54f4dc57bf07595eb8ca4984155502b96f940641
R1040604 <r1040604@student.thomasmore.be>
2026-09-19 14:27:12 +0200

    Lumiere app aangevuld

--- script.js
+++ script.js
@@ -4,3 +4,20 @@ const mobileMenu = document.querySelector(".mobile-menu");
 hamburger.addEventListener("click", () => {
     mobileMenu.classList.toggle("active");
 });
+
+const blurs = document.querySelectorAll(".blur");
+
+window.addEventListener("scroll", () => {
+
+    const scrollY = window.pageYOffset;
+
+    blurs.forEach((blur, index) => {
+
+        const speed = 0.3 + (index * 0.05);
+
+        blur.style.transform =
+            `translateY(${-scrollY * speed}px)`;
+
+    });
+
+});
